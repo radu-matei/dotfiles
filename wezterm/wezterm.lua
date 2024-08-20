2a56ee9af41eb01483c19f4ccb4cfd72d1bb2869
Radu Matei <radu.matei@fermyon.com>
2024-08-20 14:16:57 +0200

    Move Wezterm bar to bottom
    Signed-off-by: Radu Matei <radu.matei@fermyon.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -50,6 +50,9 @@ config.keys = {
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 config.adjust_window_size_when_changing_font_size = false
 config.window_padding = {
 	left   = '0cell',
@@ -57,6 +60,36 @@ config.window_padding = {
 	top    = '0cell',
 	bottom = '0cell'
 }
+
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font { family = 'DM Mono', weight = 'Medium' },
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 12.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = '#333333',
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = '#575757',
+	},
+}
+
 config.native_macos_fullscreen_mode = false
 config.audible_bell = 'Disabled'
 
